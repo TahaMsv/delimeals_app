728441d395c34da6ae8945e7c5c88270dc5ea73a
tahamousavi <tahamousavi.sbu@gmail.com>
2021-03-27 02:30:14 +0430

    Finishing the Meal List Item

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,38 @@
+import 'package:delimeals_app/widgets/meal_item.dart';
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((element) => element.categories.contains(categoryId))
+        .toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
